a9df23ab02121eb71fb60dc5d76a28f379a67d80
DongGyu Won <wdg980813@gmail.com>
2023-10-15 02:33:58 +0900

    임시

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CrawlModule } from './crawl/crawl.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), CrawlModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
